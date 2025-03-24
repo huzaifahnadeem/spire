7adbe6202c6e62b6a824a04d7f572c1685cfa67e
Huzaifah Nadeem <huzaifahnadeem@gmail.com>
2025-03-24 17:07:45 -0400

    switcher first draft

---
+++ switcher/switcher.cpp
@@ -0,0 +1,146 @@
+
+#include "switcher.h"
+
+#include <iostream>
+
+extern "C" {
+    #include "common/net_wrapper.h" 
+    #include "spines/libspines/spines_lib.h" // for spines functions e.g. spines_sendto()
+}
+
+Args args;
+std::queue <Switch_Message> pending_messages;
+
+int main(int ac, char **av) {
+    parse_args(ac, av);
+
+    // set up a spines multicast socket
+    Spines_Connection spines_connection = setup_spines_multicast_socket();
+
+    // run a thread that checks for and reads and input coming in on the named input pipe
+    pthread_t read_input_pipe_thread;
+    pthread_create(&read_input_pipe_thread, NULL, &read_input_pipe, NULL);
+
+    // run a thread that sends out any messages to the proxies
+    pthread_t proxy_messages_thread;
+    Proxy_Messages_Thread_Args message_thread_args = {.spines_conn = spines_connection};
+    pthread_create(&proxy_messages_thread, NULL, &send_pending_messages_to_proxies, (void*) &message_thread_args);
+
+    // wait for the threads before exiting
+    pthread_join(read_input_pipe_thread, NULL);
+    pthread_join(proxy_messages_thread, NULL);
+
+    return EXIT_SUCCESS;
+}
+
+void parse_args(int argc, char **argv) {
+    std::stringstream usage_stream;
+    usage_stream << "Usage: ./switcher spinesIP:port mcastIP:port proxy_info_file input_pipe_name\n";
+    std::string usage = usage_stream.str();
+
+
+    int expected_argc = 4;
+    if (argc != expected_argc) {
+        std::cout << usage;
+        exit(EXIT_FAILURE);
+    }
+
+    // parse the spines daemon address arg
+    std::string spinesd_arg = argv[1];
+    int colon_pos = -1;
+    colon_pos = spinesd_arg.find(':');
+    std::string spinesd_ipaddr = spinesd_arg.substr(0, colon_pos);
+    int spinesd_port = std::stoi(spinesd_arg.substr(colon_pos + 1));
+    args.spinesd_ipaddr = spinesd_ipaddr;
+    args.spinesd_port = spinesd_port;
+
+    // parse the arg for IP addr and the port for MCAST
+    std::string mcast_arg = argv[2];
+    colon_pos = -1;
+    colon_pos = mcast_arg.find(':');
+    std::string mcast_ipaddr = mcast_arg.substr(0, colon_pos);
+    int mcast_port = std::stoi(mcast_arg.substr(colon_pos + 1));
+    args.mcast_ipaddr = mcast_ipaddr;
+    args.mcast_port = mcast_port;
+
+    // rest of the args -- the proxy info file and input pipe name arguments:
+    args.proxy_info_file = argv[3];
+    args.input_pipe_name = argv[4];
+}
+
+void* read_input_pipe(void* fn_arg) {
+    UNUSED(fn_arg);
+    // for now just read standard input. will come back to add the whole pipe reading thing
+    std::string input;
+    while (true) {
+        std::cin >> input;
+
+        Switch_Message message_to_send = {.new_system_index = std::stoi(input)};
+        pending_messages.push(message_to_send);
+    }
+
+    
+    // int retry_wait_time_sec = 2; // if we cant access the file (e.g. if it doesnt exist, then wait for this many seconds before attempting again)
+
+    // // read the input pipe if there is something to read. if not, wait for something.
+    // while (true) {
+    //     std::ifstream pipe_file(args.input_pipe_name);
+    //     if(pipe_file.fail()) {
+    //         std::cout << "Unable to access the file \"" << args.input_pipe_name << "\". Trying again in " << retry_wait_time_sec << " seconds\n";
+    //         sleep(retry_wait_time_sec);
+    //     }
+    //     else {
+    //         break;
+    //     }
+    // }
+
+    // // create the message based on what was read on the input pipe
+    // Switch_Message message_to_send;
+    // pending_messages.push(message_to_send);
+
+    return NULL;
+}
+
+Spines_Connection setup_spines_multicast_socket() {
+    int proto, socket, reconnect_wait_time_sec;
+    proto = SPINES_RELIABLE; // options: SPINES_RELIABLE and SPINES_PRIORITY
+    reconnect_wait_time_sec = 2;
+    socket = -1;
+    while (socket < 0) {
+        socket = Spines_Mcast_SendOnly_Sock(args.spinesd_ipaddr.c_str(), args.spinesd_port, proto);
+        if (socket >= 0) break;
+        
+        // otherwise, try reconnecting
+        std::cout << "Error setting up the spines socket, Trying again in " << reconnect_wait_time_sec << " seconds\n";
+        sleep(reconnect_wait_time_sec);
+    }
+
+    Spines_Connection connection;
+    connection.socket = socket;
+    connection.dest.sin_port = htons(args.mcast_port);
+    connection.dest.sin_family = AF_INET;
+    hostent h_ent;
+    memcpy(&h_ent, gethostbyname(args.mcast_ipaddr.c_str()), sizeof(h_ent));
+    memcpy(&connection.dest.sin_addr, h_ent.h_addr, sizeof(connection.dest.sin_addr));
+
+    std::cout << "MCAST set up done\n";
+
+    return connection;
+}
+
+void* send_pending_messages_to_proxies(void* fn_args) {
+    Spines_Connection spines_connection = ((struct Proxy_Messages_Thread_Args*)fn_args)->spines_conn;
+    int ret, num_bytes;
+    while (true) {
+        if (!pending_messages.empty()) {
+            Switch_Message next_mesage = pending_messages.front();
+            pending_messages.pop();
+
+            num_bytes = sizeof(Switch_Message);
+            ret = spines_sendto(spines_connection.socket, (void *)&next_mesage, num_bytes, 0, (struct sockaddr *)&spines_connection.dest, sizeof(struct sockaddr)); 
+            if(ret != num_bytes){
+                std::cout << "Error: Spines sendto ret != num_bytes\n";
+            }
+        }
+    }
+}
---
+++ switcher/switcher.h
@@ -0,0 +1,37 @@
+#include <string>
+#include <sstream>
+#include <unistd.h> // for sleep()
+#include <queue>
+#include <fstream> // file/pipe reading
+#include <arpa/inet.h>
+#include <netdb.h> // for struct hostent
+#include <cstring> // for memcpy
+
+struct Args {
+    std::string spinesd_ipaddr;
+    int spinesd_port;
+    std::string mcast_ipaddr;
+    int mcast_port;
+    std::string proxy_info_file;
+    std::string input_pipe_name;
+};
+
+struct Spines_Connection {
+    int socket;
+    sockaddr_in dest;
+};
+
+struct Switch_Message {
+    int new_system_index;
+};
+
+struct Proxy_Messages_Thread_Args {
+    Spines_Connection spines_conn;
+};
+
+// TODO to-send message queue. read input pipe thread will put stuff in there for the other socket managment thread to read on and send
+
+void parse_args(int ac, char **av);
+void* read_input_pipe(void* fn_arg);
+Spines_Connection setup_spines_multicast_socket();
+void* send_pending_messages_to_proxies(void* fn_args);
